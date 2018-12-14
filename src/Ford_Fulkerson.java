cb6c976f07f0b044a8db3068a68f754c74ee9659
JinWonLee95 <dlwlsdnjs456@naver.com>
2018-12-15 03:17:36 +0900

    "done"

--- src/Ford_Fulkerson.java
+++ src/Ford_Fulkerson.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-public class Ford_Fulkerson  {
+public class Ford_Fulkerson {
     static final int INF = Integer.MAX_VALUE;
-    static final int V = 6;
-    static int capacity[][] =new int[][] {
+    static final int Val = 6;
+    static int capacity[][] = new int[][]{
             {0, 12, 0, 13, 0, 0},
             {0, 0, 10, 0, 0, 0},
             {0, 0, 0, 13, 3, 15},
@@ -12,55 +12,58 @@ public class Ford_Fulkerson  {
             {0, 0, 0, 0, 0, 17},
             {0, 0, 0, 0, 0, 0}
     };
-    static int parent[] = new int[V];
-    static boolean visited[] = new boolean[V];
+    static int parent[] = new int[Val];
+    static boolean visited[] = new boolean[Val];
 
-    public static void main (String[] args)    {
-        System.out.println("\n유량 네트워크의 전체 사용량 : " + fordFulkerson(0, V - 1));
+    public static void main(String[] args) {
+        System.out.println("\n전체 사용량 : " + ford_Fulkerson(0, Val - 1));
     }
 
-    public static int fordFulkerson(int source, int sink) {
+    public static int ford_Fulkerson(int source, int sink) {
         int totalFlow = 0;
         ArrayList<Integer> path = new ArrayList<>();
 
-        while(true) {
+        while (true) {
             parent[sink] = -1;
-            for(int i = 0; i < V; i++) visited[i] = false;
+            for (int i = 0; i < Val; i++) visited[i] = false;
             LinkedList<Integer> q = new LinkedList<>();
             q.add(source);
             visited[source] = true;
 
             while (q.size() != 0) {
-                int here = q.peek();
+                int now = q.peek();
                 q.pop();
-                for (int there = 0; there < V; ++there) {
-                    if (visited[there] == false && capacity[here][there] > 0) {
-                        q.add(there);
-                        parent[there] = here;
-                        visited[there] = true;
+
+                for (int i = 0; i < Val; ++i) {
+                    if (visited[i] == false && capacity[now][i] > 0) {
+                        q.add(i);
+                        parent[i] = now;
+                        visited[i] = true;
                     }
                 }
             }
 
-            if (parent[sink] == -1) break;
+            if (parent[sink] == -1)
+                break;
 
-            int amount = INF;
+            int value = INF;
+
             for (int p = sink; p != source; p = parent[p])
-                amount = Math.min(capacity[parent[p]][p], amount);
+                value = Math.min(capacity[parent[p]][p], value);
 
             for (int p = sink; p != source; p = parent[p]) {
                 path.add(parent[p]);
-                capacity[parent[p]][p] -= amount;
-                capacity[p][parent[p]] += amount;
+                capacity[parent[p]][p] -= value;
+                capacity[p][parent[p]] += value;
             }
 
-            System.out.print("## 경로 : ");
-            for(int i = path.size() - 1; i >= 0; i--)
-                System.out.print(path.get(i) + " -> ");
-            System.out.println(V - 1);
+            System.out.print(" 경로 : ");
+            for (int i = path.size() - 1; i >= 0; i--)
+                System.out.print(path.get(i) + " > ");
+            System.out.println(Val - 1);
             path = new ArrayList<>();
-            System.out.println("     최대용량 : " + amount);
-            totalFlow += amount;
+            System.out.println("     최대용량 : " + value);
+            totalFlow += value;
         }
         return totalFlow;
     }
